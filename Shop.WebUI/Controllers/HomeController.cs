fa4fe5f9da1f87ba5685428c44324fb624aeb0fe
Annvp <annvp@topica.edu.vn>
2019-11-04 00:39:34 +0700

    Filter

--- Shop.WebUI/Controllers/HomeController.cs
+++ Shop.WebUI/Controllers/HomeController.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Core.Contracts;
+using Core.Models;
+using Core.ViewModels;
 
 namespace Shop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private IBaseRepository<Product> product;
+        private IBaseRepository<ProductCategory> productCategories;
+
+        public HomeController(IBaseRepository<Product> productContext, IBaseRepository<ProductCategory> productCategoryContext)
         {
-            return View();
+            product = productContext;
+            productCategories = productCategoryContext;
+        }
+
+        public ActionResult Index(string Category = null)
+        {
+            List<Product> products;
+            List<ProductCategory> productCategory = productCategories.Collection().ToList();
+            if (Category == null)
+            {
+                products = product.Collection().ToList();
+            }
+            else
+            {
+                products = product.Collection().Where(p => p.Category == Category).ToList();
+            }
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = productCategory;
+            return View(model);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product prd = product.Find(Id);
+            if (prd == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(prd);
+            }
         }
 
         public ActionResult About()
